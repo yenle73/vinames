954acdf2b992bcc92ebb09edcb43d899a224ad53
yenle73 <128300242+yenle73@users.noreply.github.com>
2023-12-08 09:08:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import streamlit as st
+import pickle as pkl
+import numpy as np
+import sklearn
 
+class_list = {'0': 'Male', '1': 'Female'}
+input_ec = open('ec_vinames.pkl', 'rb')
+encoder = pkl.load(input_ec)
+
+input_md = open('lrc_vinames.pkl', 'rb')
+model = pkl.load(input_md)
+
+st.title('Predict Gender Based on Vietnamese Names')
+
+st.header('Enter a name')
+text = st.text_area('', '')
+
+if text != '':
+  if st.button('Predict'):
+    feature_vector = encoder.transform([text])
+    rs = str((model.predict(feature_vector))[0])
+    st.header('Result')
+    st.write(class_list[rs])
